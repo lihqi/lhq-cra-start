9f8326787cb10963be00bd922a08e2eaaba0d854
lihqi <455711093@qq.com>
2018-08-16 21:45:08 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+console.log(logo);
 class App extends Component {
   render() {
     return (
